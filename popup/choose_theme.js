8755897cc4e75fe6df3571649fa3cbf81f61c878
Jacob Baum <jacoblbaum@gmail.com>
2022-02-11 17:23:07 -0500

    Change beastify functionality

--- popup/choose_theme.js
+++ popup/choose_theme.js
@@ -11,71 +11,74 @@
 * the content script in the page.
 */
 function listenForClicks() {
-document.addEventListener("click", (e) => {
+  document.addEventListener("click", (e) => {
 
-/**
-* Given the name of a beast, get the URL to the corresponding image.
-*/
-function beastNameToURL(beastName) {        //don't need
-  switch (beastName) {
-    case "Frog":
-      return browser.runtime.getURL("beasts/frog.jpg");
-    case "Snake":
-      return browser.runtime.getURL("beasts/snake.jpg");
-    case "Turtle":
-      return browser.runtime.getURL("beasts/turtle.jpg");
+  /**
+  * Given the name of a beast, get the URL to the corresponding image.
+  */
+  function beastNameToURL(beastName) {        //don't need
+    switch (beastName) {
+      case "Frog":
+        return browser.runtime.getURL("beasts/frog.jpg");
+      case "Snake":
+        return browser.runtime.getURL("beasts/snake.jpg");
+      case "Turtle":
+        return browser.runtime.getURL("beasts/turtle.jpg");
+    }
   }
-}
 
-/**
-* Insert the page-hiding CSS into the active tab,
-* then get the beast URL and
-* send a "beastify" message to the content script in the active tab.
-*/
-function beastify(tabs) {
-  browser.tabs.insertCSS({code: hidePage}).then(() => {
-    let url = beastNameToURL(e.target.textContent);
-    browser.tabs.sendMessage(tabs[0].id, {
-      command: "beastify",
-      beastURL: url
-    });
-  });
-}
+  /**
+  * Insert the page-hiding CSS into the active tab,
+  * then get the beast URL and
+  * send a "beastify" message to the content script in the active tab.
+  */
+  function beastify(tabs) {
+    document.body.style.border = "5px solid red";
+    /*
+    browser.tabs.insertCSS({code: hidePage}).then(() => {
+        let url = beastNameToURL(e.target.textContent);
+        browser.tabs.sendMessage(tabs[0].id, {
+          command: "beastify",
+          beastURL: url
+        });
+      });
+      */
+    }
 
-/**
-* Remove the page-hiding CSS from the active tab,
-* send a "reset" message to the content script in the active tab.
-*/
-function reset(tabs) {
-  browser.tabs.removeCSS({code: hidePage}).then(() => {
+    /**
+    * Remove the page-hiding CSS from the active tab,
+    * send a "reset" message to the content script in the active tab.
+    */
+    function reset(tabs) {
+      browser.tabs.removeCSS({code: hidePage}).then(() => {
 browser.tabs.sendMessage(tabs[0].id, {
 command: "reset",
 });
+      });
+    }
+
+    /**
+    * Just log the error to the console.
+    */
+    function reportError(error) {
+      console.error(`Could not beastify: ${error}`);
+    }
+
+    /**
+    * Get the active tab,
+    * then call "beastify()" or "reset()" as appropriate.
+    */
+    if (e.target.classList.contains("themeRed")) {
+      browser.tabs.query({active: true, currentWindow: true})
+      .then(beastify)
+      .catch(reportError);
+    }
+    else if (e.target.classList.contains("reset")) {
+    browser.tabs.query({active: true, currentWindow: true})
+    .then(reset)
+    .catch(reportError);
+    }
   });
-}
-
-/**
-* Just log the error to the console.
-*/
-function reportError(error) {
-  console.error(`Could not beastify: ${error}`);
-}
-
-/**
-* Get the active tab,
-* then call "beastify()" or "reset()" as appropriate.
-*/
-if (e.target.classList.contains("beast")) {
-  browser.tabs.query({active: true, currentWindow: true})
-  .then(beastify)
-  .catch(reportError);
-}
-else if (e.target.classList.contains("reset")) {
-browser.tabs.query({active: true, currentWindow: true})
-.then(reset)
-.catch(reportError);
-}
-});
 }
 
 /**
